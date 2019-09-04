82dcb5acfeb6fcf81b34ef0455ed91c158e9a71d
xor <38231712+mountainflaw@users.noreply.github.com>
2019-09-04 13:46:04 -0400

    add switch handler

--- dedit.d
+++ dedit.d
@@ -1,1 +1,2 @@
-dedit: obj.cxx
+dedit: imgui/imgui_impl_opengl3.cpp imgui/imgui.h imgui/imconfig.h \
+ imgui/imgui_impl_opengl3.h
--- obj.cxx
+++ obj.cxx
@@ -4,25 +4,32 @@ struct Sm64Area {
     uint8_t index;
     std::string music;
     std::string collision;
-    std::string collisionType;
+    std::string terrain;
     std::string geo;
     std::string macro;
     std::string rooms;
 };
 
 struct Sm64Object {
-    uint8_t type; /* basically, anything written is an object to the editor */
-    int16_t pos[3]; /* warps: id level area */
-    int16_t rot[3]; /* warps: node*/
-    uint32_t bParam; /* warps: unused filler */
+    uint8_t type;
+    int16_t pos[3];
+    int16_t rot[3];
+    uint32_t bParam;
     uint8_t area;
-    uint8_t jump;
     bool acts[7];
-    std::string other1; /* model id or rom start */
-    std::string other2; /* bhv or rom end*/
+    std::string other1; /* model id */
+    std::string other2; /* bhv */
+};
+
+struct Sm64RomLoad {
+    uint8_t type;
+    uint8_t segment;
+    std::string romStart;
+    std::string romEnd;
 };
 
 std::vector<Sm64Object> gObjList;
+std::vector<Sm64RomLoad> gRomLoadList;
 
 class ObjHandler {
     private:
@@ -37,9 +44,7 @@ class ObjHandler {
         return wt;
     }
 
-    void parseArea();
-    void parseJump(const std::string &jump, uint8_t a);
-
+    void parseLevel(bool jump, const std::string &label);
     /** Sets matrix based off of obj properties */
     void setMtx(Sm64Object obj, int16_t mtx[4][4]) {
         mtx[0][3] = obj.pos[0];
@@ -58,7 +63,9 @@ ObjHandler::ObjHandler(const std::string &f) {
     file = f;
 }
 
-#define NUM_OBJ_TYPES 14
+#define NUM_OBJ_TYPES 16
+
+static std::string arguments[10];
 
 static const std::string objTypes[] = {
     "area_end",
@@ -75,7 +82,8 @@ static const std::string objTypes[] = {
     "load_raw",
     "load_model_from_geo",
     "mario_pos",
-    "jump_link"
+    "jump_link",
+    "return"
 };
 
 enum ObjectTypes {
@@ -93,72 +101,42 @@ enum ObjectTypes {
     OBJ_LOAD_RAW,
     OBJ_LOAD_MODEL_FROM_GEO,
     OBJ_MARIO_POS,
-    OBJ_JUMP
+    OBJ_JUMP,
+    OBJ_RETURN
 };
 
-void ObjHandler::parseJump(const std::string &jump, uint8_t a) {
-    lvlScript.open(file, std::fstream::in);
-    std::string line;
-    bool entry = false;
-    while (getline(lvlScript, line)) {
-        if (!entry && line == jump + ":") {
-            entry = true;
-            continue;
-        } else {
-            for (uint16_t i = 0; i < line.length(); i++) {
-                if (!determined) {
-                    for (uint8_t o = 0; o < NUM_OBJ_TYPES; o++) { /* parse level cmd */
-                        if (line[i] != ' ' && line.substr(i, objTypes[o].length()) == objTypes[o]) {
-                            std::cout << objTypes[o] << " set" << std::endl;
-                            //std::cout << objTypes[o].length() << std::endl;
-                            determined = true;
-                            i += objTypes[o].length();
-                            type = o;
-                            continue;
-                        }
-                    }
-                } else if (line[i] != ' ') { /* parse arguments */
-                    uint8_t distance = 0;
-                    while (line[i + distance] != ',' && i + distance < line.length()) {
-                        distance++;
-                    }
-                    argCounter++;
-                    std::cout << "argument " << line.substr(i, distance) << std::endl;
-                    i += distance;
-                }
-            }
-        }
-    }
-}
+std::string currAreaMusic = "";
+std::string currAreaCollision = "";
+std::string currAreaTerrain = "";
+std::string currAreaGeo = "";
+std::string currAreaRooms = "";
+std::string currAreaMacro = "";
+int16_t currAreaDialog[2] = {-1, -1};
 
-void ObjHandler::parseMain() {
-}
+uint8_t currAreaIndex;
+bool inArea = false;
 
-/** Initialize level script. Reads and parses level script and stores the output (if valid) into gObjectList. */
-void ObjHandler::init() {
+void ObjHandler::parseLevel(bool jump, const std::string &label) {
     std::string line;
     std::fstream lvlScript;
     uint8_t type;
-    std::string arguments[10];
 
     lvlScript.open(file, std::fstream::in);
 
     if (lvlScript.is_open()) {
         bool entryPoint = false;
-        while (getline(lvlScript, line)) {
+        while (getline(lvlScript, line)) { /* TODO: replace this with */
             uint8_t argCounter = 0;
             bool determined = false;
-            uint8_t currArea;
 
             std::regex regex(R"(\/\*.*?\*\/)"); /* strip comments */
             line = std::regex_replace(line, regex, "");
 
-            for (uint16_t i = 0; i < line.length(); i++) {
+            for (uint16_t i = 0; i < line.length(); i++) { /* extract data from line */
                 if (!determined) {
                     for (uint8_t o = 0; o < NUM_OBJ_TYPES; o++) { /* parse level cmd */
                         if (line[i] != ' ' && line.substr(i, objTypes[o].length()) == objTypes[o]) {
                             std::cout << objTypes[o] << " set" << std::endl;
-                            //std::cout << objTypes[o].length() << std::endl;
                             determined = true;
                             i += objTypes[o].length();
                             type = o;
@@ -171,8 +149,48 @@ void ObjHandler::init() {
                         distance++;
                     }
                     argCounter++;
+                    arguments[argCounter] = line.substr(i, distance);
                     std::cout << "argument " << line.substr(i, distance) << std::endl;
                     i += distance;
+                }
+
+                switch (type) {
+                case OBJ_AREA_END: /* adds previous area to area vector */
+                      break;
+                case OBJ_AREA: /* initialize area */
+                      currAreaIndex = std::stoi(arguments[0]);
+                      currAreaGeo = arguments[1];
+                      break;
+                case OBJ_OBJECT:
+                      break;
+                case OBJ_WARP_NODE:
+                      break;
+                case OBJ_MACRO_OBJECTS: currAreaMacro = arguments[0]; break;
+                case OBJ_ROOMS:
+                      break;
+                case OBJ_SET_BACKGROUND_MUSIC: currAreaMusic = arguments[0]; break;
+                case OBJ_TERRAIN_TYPE: currAreaTerrain = arguments[0]; break;
+                case OBJ_SHOW_DIALOG:
+                    currAreaDialog[0] = std::stoi(arguments[0]);
+                    currAreaDialog[1] = std::stoi(arguments[1]);
+                    break;
+                case OBJ_LOAD_MIO0_TEXTURE:
+                    gRomLoadList.push_back({OBJ_LOAD_MIO0_TEXTURE, (uint8_t)std::stoi(arguments[0]), arguments[1], arguments[2]});
+                    break;
+                case OBJ_LOAD_MIO0:
+                    gRomLoadList.push_back({OBJ_LOAD_MIO0, (uint8_t)std::stoi(arguments[0]), arguments[1], arguments[2]});
+                    break;
+                case OBJ_LOAD_RAW:
+                    gRomLoadList.push_back({OBJ_LOAD_RAW, (uint8_t)std::stoi(arguments[0]), arguments[1], arguments[2]});
+                    break;
+                case OBJ_LOAD_MODEL_FROM_GEO:
+                      break;
+                case OBJ_MARIO_POS:
+                      break;
+                case OBJ_JUMP: /* squashes calls to area if local jump, preserved if global */
+                      break;
+                case OBJ_RETURN:
+                      return;
                 }
             }
             std::cout << std::endl;
@@ -180,6 +198,12 @@ void ObjHandler::init() {
         }
         lvlScript.close();
     }
+}
+
+/** Initialize level script. Reads and parses level script and stores the output (if valid) into gObjectList. */
+void ObjHandler::init() {
+    parseLevel(false, "");
+
 }
 
 void ObjHandler::write() {
